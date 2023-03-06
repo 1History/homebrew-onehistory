31e331805bcd8dc4c29c85715c614eecca5135e9
ladit <i@ladit.me>
2023-03-06 10:53:00 +0800

    Bump to 0.3.1

--- Formula/onehistory.rb
+++ Formula/onehistory.rb
@@ -2,15 +2,15 @@ class Onehistory < Formula
   desc "All your history in one file"
   homepage "https://github.com/1History/1History"
   license "GPL-3.0"
-  version "0.3.0"
+  version "0.3.1"
 
   on_macos do
     if Hardware::CPU.intel?
       url "https://github.com/1History/1History/releases/download/v#{version}/1History_v#{version}_x86_64-apple-darwin.zip"
-      sha256 "d645ccff7dd01ca2ef36d0f1f84754936d67c59a7b92677f41130b3400982aea"
+      sha256 "699af42fc1aba3a6a3e08889ca3c076d64020ee2794d21bb088f0c1f1cba08b7"
     else
       url "https://github.com/1History/1History/releases/download/v#{version}/1History_v#{version}_aarch64-apple-darwin.zip"
-      sha256 "a7947a258324a3ba45d5bd8fb2036919b37caa903bf80af5b6b8acd07b58fda5"
+      sha256 "fd6c6ddc02dd242419cecd9f4dc964fb6cb833769cade248c523c4dbb92dda58"
     end
   end
 
